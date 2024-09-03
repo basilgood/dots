95191dc7026bf4cf37fd65436f831d3794ae14b5
basilgood <elsile69@yahoo.com>
2024-09-04 01:46:25 +0300

    chore: sound config updates

---
+++ modules/sound.nix
@@ -0,0 +1,45 @@
+{pkgs, ...}: {
+  security.rtkit.enable = true;
+  hardware.pulseaudio = {
+    enable = true;
+    support32Bit = true;
+  };
+  hardware.bluetooth.enable = true;
+  # sound.mediaKeys.enable = true;
+  # sound.enable = true;
+  # security.rtkit.enable = true;
+
+  # services.pipewire = {
+  #   enable = true;
+  #   alsa = {
+  #     enable = true;
+  #     support32Bit = true;
+  #   };
+  #   pulse.enable = true;
+  #   jack.enable = true;
+  # };
+
+  # systemd.user.services.pipewire-pulse.path = [pkgs.pulseaudio];
+
+  # services.avahi = {
+  #   enable = true;
+  #   publish.enable = true;
+  #   publish.userServices = true;
+  # };
+
+  # environment.etc."pipewire/pipewire-pulse.conf.d/50-network-party.conf".text = ''
+  #   context.exec = [
+  #     { path = "pactl" args = "load-module module-native-protocol-tcp" }
+  #     { path = "pactl" args = "load-module module-zeroconf-discover" }
+  #     { path = "pactl" args = "load-module module-zeroconf-publish" }
+  #   ]
+  # '';
+
+  environment.systemPackages = [
+    pkgs.pavucontrol
+    pkgs.ncpamixer
+    pkgs.pamixer
+    pkgs.pulsemixer
+    pkgs.pulseaudio
+  ];
+}
