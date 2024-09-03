08b4908478040c4da4ea38ed88bd68820569857f
basilgood <elsile69@yahoo.com>
2024-09-04 01:51:14 +0300

    chore: tmux config update

--- home/tmux.nix
+++ home/tmux.nix
@@ -1,26 +1,4 @@
-{pkgs, ...}: let
-  tms = pkgs.writeShellScriptBin "tms" ''
-    selected=$((find ~/Projects ~/ ~/.config -mindepth 1 -maxdepth 1 -type d ) | fzf)
-    if [[ -z $selected ]]; then
-        exit
-    fi
-    selected_name=$(basename "$selected" | tr . _)
-    tmux_running=$(pgrep tmux)
-    if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-        tmux new-session -s $selected_name -c $selected
-        exit 0
-    fi
-    if ! tmux has-session -t $selected_name 2> /dev/null; then
-        tmux new-session -ds $selected_name -c $selected
-    fi
-    if [[ -z $TMUX ]]; then
-        tmux attach-session -t $selected_name
-    else
-        tmux switch-client -t $selected_name
-    fi
-    exit
-  '';
-in {
+{pkgs, ...}: {
   programs.tmux = {
     enable = true;
     baseIndex = 1;
@@ -31,29 +9,22 @@ in {
     escapeTime = 0;
     mouse = true;
     shortcut = "Space";
+    plugins = with pkgs.tmuxPlugins; [catppuccin];
     extraConfig = ''
       unbind C-b
-      bind-key | split-window -h -c "#{pane_current_path}"
+      bind-key \\ split-window -h -c "#{pane_current_path}"
       bind-key - split-window -v -c "#{pane_current_path}"
       bind-key c new-window -c "#{pane_current_path}"
       bind-key -T copy-mode 'v' send-keys -X begin-selection
+      bind-key -r g display-popup -d '#{pane_current_path}' -w80% -h80% -E lazygit
+      set -gq allow-passthrough on
       set -ga terminal-overrides ",*col*:Tc"
-      set -g automatic-rename on
-      set -g automatic-rename-format "#{b:pane_current_path}"
       set -g set-titles on
       set -g renumber-windows on
       set -g focus-events on
-      set -g status-style bg=#1e1e1e
-      set -g status-left "#[fg=magenta,bold,bg=#1e1e1e]⮊ "
-      set -g status-right "#[fg=magenta,bold,bg=#1e1e1e]#S ⮈"
-      set -g window-status-current-style fg=magenta,bold,bg=#1e1e1e
-      set -g window-status-style fg=gray,bg=#1e1e1e,dim
-      set -g message-command-style bg=default,fg=yellow
-      set -g message-style bg=default,fg=yellow
-      set -g mode-style bg=#223249,fg=magenta
-      set -g pane-active-border-style fg=magenta,bg=default
-      set -g pane-border-style fg=brightblack,bg=default
+      set -g detach-on-destroy off
+      set -g @catppuccin_flavour 'mocha'
     '';
   };
-  home.packages = [tms];
+  home.packages = [pkgs.smug];
 }
