f8058cbe8488119e9dbd732fc2d3dfbc430b9c47
basilgood <elsile69@yahoo.com>
2024-09-04 01:41:50 +0300

    chore: i3 config update

--- home/i3.nix
+++ home/i3.nix
@@ -17,18 +17,27 @@
         outer = 0;
       };
       fonts = {
-        names = ["monospace 10"];
+        names = ["JetBrainsMono Nerd Font"];
+        size = 11.0;
+      };
+      # i3 colors
+      colors = {
+        focused = {
+          border = "#f5c2e7";
+          background = "#f5c2e7";
+          text = "#1e1e2e";
+          indicator = "#f5c2e7";
+          childBorder = "#f5c2e7";
+        };
       };
       keybindings = lib.mkOptionDefault {
-        "${modifier}+Return" = "exec ${pkgs.alacritty}/bin/alacritty";
-        "XF86AudioRaiseVolume" = "exec --no-startup-id wpctl set-sink-volume @DEFAULT_SINK@ +10%";
-        "XF86AudioLowerVolume" = "exec --no-startup-id wpctl set-sink-volume @DEFAULT_SINK@ -10%";
-        "XF86AudioMute" = "exec --no-startup-id wpctl set-sink-mute @DEFAULT_SINK@ toggle";
-        "XF86AudioMicMute" = "exec --no-startup-id wpctl set-source-mute @DEFAULT_SOURCE@ toggle";
+        "${modifier}+Return" = "exec kitty";
+        "XF86AudioRaiseVolume" = "exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ +10%";
+        "XF86AudioLowerVolume" = "exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ -10%";
+        "XF86AudioMute" = "exec --no-startup-id pactl set-sink-mute @DEFAULT_SINK@ toggle";
+        "XF86AudioMicMute" = "exec --no-startup-id pactl set-source-mute @DEFAULT_SOURCE@ toggle";
         "${modifier}+Shift+e" = "exec rofi -show p -modi p:rofi-power-menu";
-        "${modifier}+d" = "exec rofi -show drun -show-icons -terminal alacritty";
-        "Print" = "exec --no-startup-id ${pkgs.rofi-screenshot}/bin/rofi-screenshot";
-        "Shift+Print" = "exec --no-startup-id ${pkgs.rofi-screenshot}/bin/rofi-screenshot -s";
+        "${modifier}+d" = "exec rofi -show drun -show-icons -terminal kitty";
         "${modifier}+l" = "exec --no-startup-id betterlockscreen --lock blur";
         "${modifier}+z" = "exec --no-startup-id ${pkgs.keepmenu}/bin/keepmenu";
         "${modifier}+Shift+quotedbl" = "kill";
@@ -45,7 +54,7 @@
           command = "floating enable";
         }
         {
-          criteria.class = "vokoscreenNG|Thunar";
+          criteria.class = "vokoscreenNG|Thunar|arandr|mpv|btop|SimpleScreenRecorder";
           command = "floating enable";
         }
         {
@@ -71,11 +80,33 @@
           statusCommand = "${config.programs.i3status-rust.package}/bin/i3status-rs ${config.xdg.configHome}/i3status-rust/config-bottom.toml";
           command = "i3bar";
           fonts = {
-            names = ["monospace"];
+            names = ["JetBrainsMono Nerd Font"];
             size = 12.0;
           };
+          # bar colors
           colors = {
-            background = "#1e1e1e";
+            background = "#1e1e2e";
+            statusline = "#cdd6f4";
+            focusedWorkspace = {
+              border = "#b4befe";
+              background = "#f5c2e7";
+              text = "#1e1e2e";
+            };
+            activeWorkspace = {
+              border = "#b4befe";
+              background = "#5f676a";
+              text = "#cdd6f4";
+            };
+            inactiveWorkspace = {
+              border = "#6c7086";
+              background = "#302d41";
+              text = "#cdd6f4";
+            };
+            urgentWorkspace = {
+              border = "#eba0ac";
+              background = "#fab387";
+              text = "#1e1e2e";
+            };
           };
         }
       ];
