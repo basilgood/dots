f584e67b054af255d22a69d5f98f99ce777db3aa
basilgood <elsile69@yahoo.com>
2024-09-04 01:49:15 +0300

    chore: virtualization config

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -1,3 +1,16 @@
 _: {
   virtualisation.docker.enable = true;
+  # virtualisation = {
+  #   podman = {
+  #     enable = true;
+
+  #     # Create a `docker` alias for podman, to use it as a drop-in replacement
+  #     dockerCompat = true;
+
+  #     # Required for containers under podman-compose to be able to talk to each other.
+  #     defaultNetwork.settings.dns_enabled = true;
+  #     dockerSocket.enable = true;
+  #   };
+  #   vmware.host.enable = true;
+  # };
 }
