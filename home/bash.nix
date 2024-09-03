c49f5aba980aeaf7d33b3c610555d91eaaa903ba
basilgood <elsile69@yahoo.com>
2024-09-04 01:39:51 +0300

    chore: bash config update

--- home/bash.nix
+++ home/bash.nix
@@ -3,33 +3,14 @@
     enable = true;
     shellAliases = {
       grep = "grep --color=auto";
-      ls = "ls --color=tty";
-      ll = "ls -l";
-      l = "ls -alh";
       lt = "${pkgs.tree}/bin/tree -L 1";
-      ha = "history -n";
+      hm = "sed 's/[[:space:]]*$//' $HISTFILE | tac | awk '!x[$0]++' | tac | ${pkgs.moreutils}/bin/sponge $HISTFILE";
     };
     historySize = -1;
     historyFileSize = -1;
     historyControl = ["ignoredups" "erasedups"];
-    historyIgnore = [
-      "ls"
-      "exit"
-      "history"
-      "kill"
-      "fg"
-      "bg"
-      "mpv"
-    ];
-    shellOptions = [
-      "nocaseglob"
-      "autocd"
-      "dirspell"
-      "cdspell"
-      "cmdhist"
-      "histappend"
-    ];
-
+    historyIgnore = ["l" "ls" "ll" "lt" "cd" "rm" "rm rf" "hm" "yt" "exit" "history" "kill" "pkill" "fg" "bg" "mpv" "aria2c" "yazi"];
+    shellOptions = ["nocaseglob" "autocd" "dirspell" "cdspell" "cmdhist" "histappend"];
     initExtra = ''
       set -o notify
       bind "set completion-ignore-case on"
@@ -46,11 +27,7 @@
       bind '"\e[Z": menu-complete-backward'
       bind '"\e[A": history-search-backward'
       bind '"\e[B": history-search-forward'
-      bind '"\e[M": kill-word'
       bind '"\C-h": backward-kill-word'
-      hm() {
-        sed 's/[[:space:]]*$//' $HISTFILE | tac | awk '!x[$0]++' | tac | ${pkgs.moreutils}/bin/sponge $HISTFILE
-      }
       stty -ixon
     '';
 
@@ -61,30 +38,37 @@
       Y="\[\e[33m\]"
       B="\[\e[34m\]"
       P="\[\e[35m\]"
-      ARW="⤚ "
+
+      PROMPT_SYMBOL="› "
+      JBV_SYMBOL="•"
+      NIX_SYMBOL="❄-"
+
       _set_bash_prompt() {
-        EXIT="$?"
-        [ $EXIT -eq 0 ] && err="$B" || err="$R"
-        [ $(jobs -p | wc -l) -ne 0 ] && JBV=" $Y•$N" || JBV=""
-        [ -n "$IN_NIX_SHELL" ] && NIX=" $B❄-$IN_NIX_SHELL$N"
+        local EXIT=$?
+        local ERR=$([[ $EXIT -eq 0 ]] && echo "$B" || echo "$R")
+        local JBV=$([[ $(jobs -p | wc -l) -ne 0 ]] && echo " $Y$JBV_SYMBOL$N" || echo "")
+        local NIX=$([[ -n "$IN_NIX_SHELL" ]] && echo " $B$NIX_SYMBOL$IN_NIX_SHELL$N" || echo "")
+
         if [ ! -d "$PWD/.git" ]; then
-          PS1="$BD$P\W$N$NIX$JBV\n$err$ARW$N"
-          PS2="↪︎$NC"
+          PS1="$BD$P\W$N$NIX$JBV\n$ERR$PROMPT_SYMBOL$N"
+          PS2="↪︎$N"
         else
-          M=" "
-          ref=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
-          [[ "$ref" == "HEAD" ]] && ref='detached*'
-          status="$(git status --porcelain -b 2>/dev/null)"
-          [[ $status =~ ([[:cntrl:]][A-Z][A-Z\ ]\ ) ]] && M+="+"
-          [[ $status =~ ([[:cntrl:]][A-Z\ ][A-Z]\ ) ]] && M+='٭'
-          [[ $status =~ ([[:cntrl:]]\?\?\ ) ]] && M+='?'
-          [[ -e "$PWD/.git/refs/stash" ]] && M+='≡'
-          [[ $status =~ ahead\ ([0-9]+) ]] && M+=''${BASH_REMATCH[1]}'⇡'
-          [[ $status =~ behind\ ([0-9]+) ]] && M+=''${BASH_REMATCH[1]}'⇣'
-          PS1="$BD$P\W$N $Y$N $BD$B$ref$R$M$N$NIX$JBV\n$err$ARW$N"
-          PS2="↪︎$NC"
+          local M=" "
+          local head=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
+          local status=$(git status --porcelain -b 2>/dev/null)
+
+          M+=$( [[ $status =~ ([[:cntrl:]][A-Z][A-Z\ ]\ ) ]] && echo "+")
+          M+=$( [[ $status =~ ([[:cntrl:]][A-Z\ ][A-Z]\ ) ]] && echo "٭")
+          M+=$( [[ $status =~ ([[:cntrl:]]\?\?\ ) ]] && echo "?")
+          M+=$( [[ -e "$PWD/.git/refs/stash" ]] && echo "≡")
+          M+=$( [[ $status =~ ahead\ ([0-9]+) ]] && echo "''${BASH_REMATCH[1]}⇡")
+          M+=$( [[ $status =~ behind\ ([0-9]+) ]] && echo "''${BASH_REMATCH[1]}⇣")
+
+          PS1="$BD$P\W$N $Y$N $BD$B$head$R$M$N$NIX$JBV\n$ERR$PROMPT_SYMBOL$N"
+          PS2="↪︎$N"
         fi
       }
+
       PROMPT_COMMAND=_set_bash_prompt
     '';
   };
