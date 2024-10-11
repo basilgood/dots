988cb6cac671146e02c80596b79ec50dd40b3a62
basilgood <elsile69@yahoo.com>
2024-10-12 01:15:24 +0300

    chore(xdg): config update

--- home/xdg.nix
+++ home/xdg.nix
@@ -9,6 +9,11 @@
   ];
   xdg = {
     enable = true;
+    portal = {
+      enable = true;
+      extraPortals = [pkgs.xdg-desktop-portal-gtk];
+      config.common.default = "gtk";
+    };
     configFile."mimeapps.list".force = true;
 
     cacheHome = "${config.home.homeDirectory}/.cache";
@@ -19,33 +24,20 @@
     mimeApps = {
       enable = true;
       defaultApplications = let
-        browser = ["brave.desktop"];
-        editor = ["nvim.desktop"];
+        browser = "brave.desktop";
+        editor = "nvim.desktop";
+        mpv = "mpv.desktop";
+        photo = "qimgv.desktop";
       in {
-        "application/json" = browser;
+        "application/json" = [editor];
+        "text/*" = [editor];
+        "text/html" = [browser];
         "application/pdf" = ["org.pwmt.zathura.desktop"];
-
-        "text/html" = browser;
-        "text/xml" = browser;
-        "text/plain" = editor;
-        "application/xml" = browser;
-        "application/xhtml+xml" = browser;
-        "application/xhtml_xml" = browser;
-        "application/rdf+xml" = browser;
-        "application/rss+xml" = browser;
-        "application/x-extension-htm" = browser;
-        "application/x-extension-html" = browser;
-        "application/x-extension-shtml" = browser;
-        "application/x-extension-xht" = browser;
-        "application/x-extension-xhtml" = browser;
-        "application/x-wine-extension-ini" = editor;
-
         "x-scheme-handler/discord" = ["discord.desktop"];
         "x-scheme-handler/tg" = ["org.telegram.desktop.desktop "];
-
-        "audio/*" = ["mpv.desktop"];
-        "video/*" = ["mpv.desktop"];
-        "image/*" = ["qimgv.desktop"];
+        "audio/*" = mpv;
+        "video/*" = mpv;
+        "image/*" = photo;
       };
     };
   };
