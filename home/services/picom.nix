067358395954e1f2596e72de05f241682d6b1cd7
basilgood <elsile69@yahoo.com>
2024-11-11 00:41:43 +0200

    chore: picom config

--- home/services/picom.nix
+++ home/services/picom.nix
@@ -1,63 +1,53 @@
-{pkgs, ...}: let
-  my_picom = pkgs.picom.overrideAttrs (oa: {
-    src = pkgs.fetchgit {
-      url = "https://github.com/yshui/picom";
-      rev = "6caa76a281e906784deb05038e38919e878be6e0";
-      hash = "sha256-05jiIVNubAv8Dby/Tbkag6uB4U/aX58gTKI2NxbKqCU";
-    };
-    nativeBuildInputs =
-      (oa.nativeBuildInputs or [])
-      ++ [
-        pkgs.pcre
-      ];
-  });
-in {
+{pkgs, ...}: {
   services.picom = {
     enable = true;
-    package = my_picom;
-    vSync = true;
+    package = pkgs.picom-next;
     backend = "glx";
-
+    vSync = true;
+    shadow = true;
+    shadowExclude = ["window_type *= 'menu'" "name ~= 'Firefox$'" "name ~= 'Brave-browser$'" "focused = 1"];
+    # Fading
+    fade = true;
+    fadeDelta = 10;
     settings = {
       animations = true;
-      animation-window-mass = 1;
-      animation-dampening = 20;
-      animation-stiffness = 250;
-      animation-clamping = false;
+      animation-stiffness-in-tag = 125;
+      animation-stiffness-tag-change = 90.0;
+      animation-window-mass = 0.4;
+      animation-dampening = 15;
+      animation-clamping = true;
+      detect-transient = true;
+      mark-wmwin-focused = true;
+      #open windows
       animation-for-open-window = "zoom";
-      animation-for-unmap-window = "zoom";
-      animation-for-transient-window = "slide-down";
+      #minimize or close windows
+      animation-for-unmap-window = "squeeze";
+      animation-for-transient-window = "slide-up"; #available options: slide-up, slide-down, slide-left, slide-right, squeeze, squeeze-bottom, zoom
 
-      fade-in-step = 0.028;
-      fade-out-step = 0.028;
+      #set animation for windows being transitioned out while changings tags
+      animation-for-prev-tag = "minimize";
+      #enables fading for windows being transitioned out while changings tags
+      enable-fading-prev-tag = true;
 
-      focus-exclude = ["class_g ?= 'rofi'" "class_g ?= 'Steam'"];
-      rounded-corners-exclude = [
-        "! name~=''"
-        "window_type = 'dock'"
-        "window_type = 'desktop'"
-        "class_g ?= 'Dunst'"
+      #set animation for windows being transitioned in while changings tags
+      animation-for-next-tag = "slide-in-center";
+      #enables fading for windows being transitioned in while changings tags
+      enable-fading-next-tag = true;
+      blur = {
+        method = "dual_kawase";
+        strength = 8;
+      };
+      blur-background-exclude = [
+        "class_g = 'slop'"
       ];
-
-      corner-radius = 10;
-      round-borders = 0;
-      round-borders-exclude = [
-        "! name~=''" # Qtile == empty wm_class..
-      ];
-
-      detect-rounded-corners = true;
-      detect-client-opacity = true;
-
-      invert-color-include = [];
-      glx-no-stencil = true;
-      use-damage = false;
-      transparent-clipping = false;
-      fading = true;
-    };
-
-    wintypes = {
-      dock = {animation = "slide-down";};
-      toolbar = {animation = "slide-down";};
+      popup_menu = {
+        opacity = 1.0;
+        shadow = false;
+        full-shadow = false;
+        focus = false;
+      };
+      corner-radius = 4;
+      round-borders = 1;
     };
   };
 }
